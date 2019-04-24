ce75116ccd19c0071d8290cabe6f4249a5fddb5b
HelloWorld945 <1831862846@qq.com>
2019-04-24 17:23:02 +0800

    Initial commit

---
+++ Module01/src/com/study/Hello.java
@@ -0,0 +1,28 @@
+package com.study;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Hello {
+    private String name;
+
+    public Hello() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World!----Module01");
+        List list = new ArrayList();
+
+    }
+}
